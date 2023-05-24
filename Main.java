1ed1d74a8b5dceb1b6168e9500bc58ecb4fb5959
Mateus Abdallah <abdallahmateus@gmail.com>
2023-05-23 23:32:20 -0700

    creating some comments

--- Main.java
+++ Main.java
@@ -7,7 +7,10 @@ public class Main {
        Scanner input = new Scanner(System.in);
        double unitsConsumed;
        int selection;
-
+       
+       //Getting the number of units consumed
+       //And computing the total bill amount
+       
        System.out.println("Enter the number of units consumed do you have:");
        unitsConsumed = input.nextInt();
        if(unitsConsumed <= 200){
@@ -20,7 +23,9 @@ public class Main {
            unitsConsumed *= 5;
            System.out.println("The total bill amount is $"+ unitsConsumed);
        }
-
+       
+       //Analyzing if the bill will be paid within 15 days or after 15 days
+       
        System.out.println("Do you going to pay within 15 days?");
        System.out.println("1 - Yes");
        System.out.println("2 - No");
@@ -37,20 +42,24 @@ public class Main {
         }
     }
 
+    //Method to compute the discount of 10%
+    
     private static void discount10(double unitsConsumed){
     	System.out.println("---------------");
         System.out.println("Payment within 15 days will have a discount of 10%");
         double discount = unitsConsumed * 0.1;
         double adjustedBill = unitsConsumed - discount;
-        System.out.println("The total bill adjusted with discount is $"+ adjustedBill);
+        System.out.println("So the total bill adjusted with discount is $"+ adjustedBill);
         System.out.println("---------------");
     }
+    
+    //Method to compute the surcharge of 5%
 
     private  static void surCharge5(double unitsConsumed){
     	System.out.println("---------------");
         System.out.println("Payment after 15 days will have a surcharge of 5%");
         double surCharge = unitsConsumed * 1.05;
-        System.out.println("The total bill adjusted with surcharge is $" + surCharge);
+        System.out.println("So the total bill adjusted with surcharge is $" + surCharge);
         System.out.println("---------------");
     }
 }
